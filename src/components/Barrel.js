feafb7fdc3f91bc8f7c6166ef1bb5e1ff54c7d2e
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-24 09:29:04 -0700

    create barrel props and proptypes

---
+++ src/components/Barrel.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Barrel(props){
+  
+  return (
+    <React.Fragment>
+      <div onClick = {() => props.whenBarrelClicked(props.id)}>
+      <h3>{props.type}</h3>
+      <h5>{props.brand}</h5>
+      <h5>{props.price}</h5>
+      <h5>{props.alcoholContent}</h5>
+      </div>
+    </React.Fragment>
+  );
+}
+
+Barrel.propTypes = {
+  type: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+  alcoholContent: PropTypes.number,
+  id: PropTypes.string,
+  whenBarrelClicked: PropTypes.func
+};
+
+export default Barrel; 
---
+++ src/components/BarrelList.js
@@ -0,0 +1,4 @@
+
+import React from "react";
+import Barrel from "./Barrel";
+import PropTypes from "prop-types";
