c2a63e00dc02601fe9a10088a6bcfc53ccaf2304
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-04-24 09:35:09 -0700

    add barrel list array functionality

--- src/components/BarrelList.js
+++ src/components/BarrelList.js
@@ -1,4 +1,28 @@
-
 import React from "react";
 import Barrel from "./Barrel";
 import PropTypes from "prop-types";
+
+function BarrelList(props) {
+  return(
+    <React.Fragment>
+      <hr/>
+      {props.barrelList.map((barrel) => 
+      <Barrel
+      whenBarrelClicked = {props.onBarrelSelection}
+      type = {barrel.type}
+      name = {barrel.name}
+      price = {barrel.price}
+      alcoholContent = {barrel.alcoholContent}
+      id = {barrel.id}
+      key = {barrel.id} />
+      )}
+    </React.Fragment>
+  );
+}
+
+BarrelList.propTypes = {
+  barrelList: PropTypes.array,
+  onBarrelSelection: PropTypes.func,
+};
+
+export default BarrelList; 
