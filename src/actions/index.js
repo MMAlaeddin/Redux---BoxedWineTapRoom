d092eef070cc1de52f9c7bd59ec8d9eabede196d
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-01 13:16:41 -0700

    pass add barrel action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,16 @@ export const deleteBarrel = id => ({
 export const toggleForm = () => ({
   type: 'TOGGLE_FORM'
 });
+
+export const addBarrel = (barrel) => {
+  const { wineType, name, price, alcoholContent, quantity, id } = barrel;
+  return {
+    type: "ADD_BARREL",
+    wineType: wineType,
+    name: name,
+    price: price,
+    alcoholContent: alcoholContent,
+    quantity: quantity,
+    id: id
+  }
+}
