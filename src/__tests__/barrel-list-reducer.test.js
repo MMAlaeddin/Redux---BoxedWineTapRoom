6d94d6f01a18ef5d17957729b6f4887fb4ae522c
Mariam Alaeddin <mariamalaeddin@gmail.com>
2020-05-01 08:30:33 -0700

    pass default state test

--- src/__tests__/barrel-list-reducer.test.js
+++ src/__tests__/barrel-list-reducer.test.js
@@ -1,6 +1,15 @@
-import barrelListReducer from "../../reducers/barrel-list-reducer";
+import barrelListReducer from "../reducers/barrel-list-reducer";
 
 describe ("barrelListReducer", () => {
+
+  // let action; 
+  // const barrelData = {
+  //   type: "Cab",
+  //   name: "Purple Farms",
+  //   price: "$30",
+  //   alcoholContent: "15%",
+  //   quantity: 150
+  // }; 
   test ("Should return default state if there is no action type passed into the reducer", () => {
     expect (barrelListReducer({}, { type: null })).toEqual({});
   })
